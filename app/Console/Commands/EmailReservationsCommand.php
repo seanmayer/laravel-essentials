4d72b3595e9c591bd68da9bf1db71a151df06a6b
Sean Mayer Laptop <m014609f@student.staffs.ac.uk>
2020-04-27 18:03:53 +0100

    Artisan Console - Console commands(Viewed) - Save Console commands - Console outputs(Viewed) - Save Console outputs - Console arguments(Viewed) - Save Console arguments - Console options(Viewed) - Save Console options - Console inputs(Viewed)

---
+++ app/Console/Commands/EmailReservationsCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class EmailReservationsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reservations:notify 
+    {count : The number of bookings to retrieve}
+    {--dry-run= : To have this command do no actual work.}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Notify reservations holders';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $answer = $this->choice('What service should we use?', 
+                                    ['sms','email'],'email'
+                                   );
+        var_dump($answer);
+        $count = $this->argument('count');
+        if (!is_numeric($count)){
+            $this->alert('The count must be a number');
+            return 1;
+        }
+        $bookings = \App\Booking::with(['room.roomType', 'users'])->limit($count)->get();
+        $this->info(sprintf('The number of bookings to alert for is: %d', $bookings->count()));
+        $bar = $this->output->CreateProgressBar($bookings->count());
+        $bar->start();
+        foreach($bookings as $booking){
+            if($this->option('dry-run')){
+                $this->info('Would process booking');
+            } else {
+                $this->error('Nothing happened');
+            }
+            $bar->advance();
+        }
+        $bar->finish();
+        $this->comment('Command completed!');
+    }
+}
